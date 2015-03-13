f3266027bcec4c2ed819cc1ee0844eef95aee103
Luca Zanussi <luca.z@outlook.com>
2015-03-14 00:14:21 +0100

    Minor EOF fixes and added *dummy* engine
    But... can I remember to add newlines on each source file created by now on? (Dumb Visual Studio)

---
+++ GL Cookbook/src/engines/chapter_1/1_GettingStarted.cpp
@@ -0,0 +1,28 @@
+﻿#include "1_GettingStarted.h"
+
+using namespace std;
+
+void GettingStartedEngine::onInit()
+{
+	glClearColor(1.0f, 0.7f, 0, 0); // Alpha channel only used in alpha blending
+}
+
+void GettingStartedEngine::onRender()
+{
+	// Clear color and depth buffer to the clear color and clear depth values.
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Current back buffer is set as the current front buffer, shown on the screen.
+	// Required in a double buffered OpenGl application.
+	glutSwapBuffers();
+}
+
+void GettingStartedEngine::onResize(int w, int h)
+{
+	cout << "Size: " << w << ' ' << h << '\n';
+}
+
+void GettingStartedEngine::onShutdown()
+{
+	cout << "Shutdown successful\n";
+}
---
+++ GL Cookbook/src/engines/chapter_1/1_GettingStarted.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "../GLEngine.h"
+
+class GettingStartedEngine : public GLEngine
+{
+public:
+	GettingStartedEngine() : GLEngine(GLE_REGISTER_DISPLAY) { }
+	void onInit() override;
+
+private:
+	void onRender() override;
+	void onResize(int w, int h) override;
+	void onShutdown() override;
+
+};
