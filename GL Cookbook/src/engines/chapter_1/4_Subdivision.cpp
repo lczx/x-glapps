6fc3b944af46b23d2dc42b9bfe803fa68de6d0b0
Luca Zanussi <luca.z@outlook.com>
2015-03-16 21:20:28 +0100

    Initial work on geometry shader tests

---
+++ GL Cookbook/src/engines/chapter_1/4_Subdivision.cpp
@@ -0,0 +1,30 @@
+﻿#include "4_Subdivision.h"
+
+SubdivisionEngine::SubdivisionEngine() : GLEngine(GLE_REGISTER_ALL ^ GLE_REG_IDLE)
+{
+	
+}
+
+void SubdivisionEngine::onRender()
+{
+}
+
+void SubdivisionEngine::onResize(int w, int h)
+{
+}
+
+void SubdivisionEngine::onShutdown()
+{
+}
+
+void SubdivisionEngine::onMouseDown(int button, int s, int x, int y)
+{
+}
+
+void SubdivisionEngine::onMouseMove(int x, int y)
+{
+}
+
+void SubdivisionEngine::onKey(unsigned char key, int x, int y)
+{
+}
